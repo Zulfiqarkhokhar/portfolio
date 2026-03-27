bd3301247068d0b3a7b500b6cf255f1fc51a67de
ZulfiqarAli111 <zulfiqarali.bscssef20@iba-suk.edu.pk>
2026-03-27 12:33:03 +0500

    file updated

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  FiTInstagram,
   FiGithub,
   FiLinkedin,
   FiInstagram,
@@ -12,21 +11,21 @@ const Social = () => {
       <a
         href="https://www.instagram.com/zulfiqarkhokhar00/"
         className="home__social-icon"
-        target="_blank"
+        target="_blank" rel="noreferrer"
       >
         <FiInstagram />
       </a>
       <a
         href="https://github.com/Zulfiqarkhokhar"
         className="home__social-icon"
-        target="_blank"
+        target="_blank" rel="noreferrer"
       >
         <FiGithub />
       </a>
       <a
         href="https://www.linkedin.com/in/zulfiqar-ali-khokhar-93114925b/"
         className="home__social-icon"
-        target="_blank"
+        target="_blank" rel="noreferrer"
       >
         <FiLinkedin />
       </a>
